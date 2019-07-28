1da977df7f307c2c6fe70df6ff650f9bb9038032
asvirin <45921969+asvirin@users.noreply.github.com>
2019-07-28 21:58:45 +0300

    Update tg-bot.py

--- tg-bot.py
+++ tg-bot.py
@@ -7,6 +7,9 @@ import logging
 LIST_ANSWERS = os.environ['LIST_ANSWERS']
 LIST_STICKERS = os.environ['LIST_STICKERS']
 
+global LIST_ANSWERS
+global LIST_STICKERS
+
 class MyLogsHandler(logging.Handler):
     def emit(self, record):
         telegram_bot_information_token = os.environ['TELEGRAM_BOT_INFORMATION_TOKEN']
@@ -16,24 +19,33 @@ class MyLogsHandler(logging.Handler):
         bot_error.send_message(chat_id=chat_id_telegram_information, text=log_entry)
 
 def echo_text(bot, update):
-    chat_id = update.message.chat_id
-    bot_answer = os.environ['STANDART_TEXT_PHRASE']
-    update.message.reply_text(bot_answer)
+    try:
+        chat_id = update.message.chat_id
+        bot_answer = os.environ['STANDART_TEXT_PHRASE']
+        update.message.reply_text(bot_answer)
+    except Exception as error:
+        raise MyLogsHandler(error)
         
 def echo_photo(bot, update):
     chat_id = update.message.chat_id
     sticker_or_text = random.randint(0, 1)
-    if sticker_or_text == 0:
-        bot_answer_number = random.randint(0, len(LIST_ANSWERS))
-        bot_answer = LIST_ANSWERS[bot_answer_number]
-        update.message.reply_text(bot_answer)
-    else:
-        bot_answer_number = random.randint(0, len(LIST_STICKERS))
-        bot_answer = LIST_STICKERS[bot_answer_number]
-        bot.sendSticker(chat_id = chat_id, sticker = bot_answer)
+    try:
+        if sticker_or_text == 0:
+            bot_answer_number = random.randint(0, len(LIST_ANSWERS))
+            bot_answer = LIST_ANSWERS[bot_answer_number]
+            update.message.reply_text(bot_answer)
+        else:
+            bot_answer_number = random.randint(0, len(LIST_STICKERS))
+            bot_answer = LIST_STICKERS[bot_answer_number]
+            bot.sendSticker(chat_id = chat_id, sticker = bot_answer)
+    except Exception as error:
+        raise MyLogsHandler(error)
     
 def start(bot, update):
-    update.message.reply_text(os.environ['START_PHRASE'])
+    try:
+        update.message.reply_text(os.environ['START_PHRSE'])
+    except Exception as error:
+        raise MyLogsHandler(error)
 
 if __name__ == '__main__': 
     telegram_token = os.environ['TELEGRAM_TOKEN']
